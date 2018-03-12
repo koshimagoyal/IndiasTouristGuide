7a3b1c3eee310df924bf8199e8781dae0a37eb93
koshimagoyal <koshimagoyal@yahoo.co.in>
2018-03-12 23:02:38 +0530

    Add existing file

--- IndiasTouristGuide/app/src/main/java/com/example/lenovo/indiastouristguide/North.java
+++ IndiasTouristGuide/app/src/main/java/com/example/lenovo/indiastouristguide/North.java
@@ -4,13 +4,19 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
 
 public class North extends AppCompatActivity {
 
+    public LinearLayout click;
+    public View shanta;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_north);
 
+
     }
+
 }
